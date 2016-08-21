49a04cfc42cbc16b40e0708901c6778767573d4b
Hakchin Kim <sn2ro@eerfmai.local>
2016-08-21 13:52:34 +0900

    abc

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func btnSend(sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
     }
 }
 
